eb78b95dfb7287d37c5351a3417aa5c19fda368e
dan <dan@conjecture.dev>
2024-12-06 17:32:27 +0000

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let counter ;
 
 const incrementBy1 = async () => {
+  // replace this with your own fetch by clicking "manage" after hovering over the incrementBy1 tactic in your workspace
   const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -12,6 +13,7 @@ const incrementBy1 = async () => {
 } ;
 
 const incrementBy2 = async () => {
+  // replace this with your own fetch by clicking "manage" after hovering over the incrementBy2 tactic in your workspace
   const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -23,6 +25,7 @@ const incrementBy2 = async () => {
 } ;
 
 const fetchCounter = async () => {
+  // replace this with your own fetch by clicking "manage" after hovering over the fetchCounter tactic in your workspace
   const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
